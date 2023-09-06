cc39b0af98adb149b31fadd82f478f5504449de7
suhailhameed919 <suhailhameed919@gmail.com>
2023-09-06 00:13:16 -0400

    login functionality backend

--- back-end/user_app/models.py
+++ back-end/user_app/models.py
@@ -6,14 +6,14 @@ from django.contrib.auth.models import Group, Permission
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    name = models.CharField()
+    name = models.CharField() 
     annual_salary = models.BigIntegerField()
     bank_balance = models.BigIntegerField()
     debt = models.BigIntegerField()
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ['username']
     
-    groups = models.ManyToManyField(Group, related_name='custom_user_set')
-    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_set')
+    # groups = models.ManyToManyField(Group, related_name='custom_user_set')
+    # user_permissions = models.ManyToManyField(Permission, related_name='custom_user_set')
     def __str__(self):
         return f"{self.email}"
