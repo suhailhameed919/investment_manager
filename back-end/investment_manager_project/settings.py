7b4b717b0939180a8ef6fd56d48f3741395b9c1c
suhailhameed919 <suhailhameed919@gmail.com>
2023-09-07 14:40:24 -0400

    middleware order

--- back-end/investment_manager_project/settings.py
+++ back-end/investment_manager_project/settings.py
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'investment_manager_project.urls'
@@ -131,6 +131,8 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'user_app.User'
+
+#below changes have not been pushed, (commited)
 
 # CORS_ALLOWED_ORIGINS = [
 #     'http://localhost:5173',
